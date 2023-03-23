82a66f3988cbf536654926124e455a2941c8f019
owlowka <roksana.maria.duda@gmail.com>
2023-03-23 22:20:56 +0100

    Zadeklarowanie zmiennych z imieniem, płcią, wiekiem. Stworzenie algorytmu do weryfikacji danych.

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool woman = true;
+int age = 28;
+
+if ( woman == true)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine($"{name}, lat {age}");
+    }
+}
+else if ( woman == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
