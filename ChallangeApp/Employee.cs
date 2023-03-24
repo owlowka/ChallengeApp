00e6a840bd103209e9480d2a233856a1203b01ab
owlowka <roksana.maria.duda@gmail.com>
2023-03-24 21:48:24 +0100

    Utworzenie klasy Pracownik . Utworzenie metody wypisujacej dane najlepszego pracownika.

---
+++ ChallangeApp/Employee.cs
@@ -0,0 +1,42 @@
+﻿
+namespace ChallangeApp
+{
+    internal class Employee
+    {
+        private List <int> score = new List <int> ();
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public Employee() 
+        {
+            
+        }
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public void AddScore(int number)
+        {
+            if (number >= 1 && number <= 10)
+            {
+                this.score.Add(number);
+            }
+            else
+            {
+                Console.WriteLine($"Pracownik: {Name} Nieprawidłowa ocena. Zakres ocen od 1-10");
+            }
+        }
+
+    }
+}
--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,17 +1,46 @@
 ﻿
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-int[] digits = new int[10];
 
-foreach (char letter in letters)
-{
-    int digit = int.Parse(letter.ToString());
-    digits[digit]++;
-}
+using ChallangeApp;
 
-for (int i = 0;i < digits.Length; i++)
+Employee employee1 = new Employee("Roksana", "Talisman", 30);
+Employee employee2 = new Employee("Waldek", "Tajniak", 28);
+Employee employee3 = new Employee("Mietek", "Atlas", 26);
+
+employee1.AddScore(5);
+employee1.AddScore(8);
+employee1.AddScore(1);
+employee1.AddScore(4);
+employee1.AddScore(8);
+
+employee2.AddScore(1);
+employee2.AddScore(3);
+employee2.AddScore(6);
+employee2.AddScore(9);
+employee2.AddScore(8);
+
+employee3.AddScore(10);
+employee3.AddScore(8);
+employee3.AddScore(9);
+employee3.AddScore(8);
+employee3.AddScore(9);
+
+int result1 = employee1.Result;
+int result2 = employee2.Result;
+int result3 = employee3.Result;
+
+List<Employee> employees = new List<Employee>()
 {
-    Console.WriteLine($"{i} => {digits[i]}");
-}
+    employee1,
+    employee2,
+    employee3
+};
+
+Employee? bestEmployee = employees.MaxBy(employee => employee.Result);
+
+Console.WriteLine($"Najlepszy pracownik: " +
+    $"{bestEmployee.Name} " +
+    $"{bestEmployee.Surname} " +
+    $"{bestEmployee.Surname} " +
+    $"{bestEmployee.Age} " +
+    $"Wynik: {bestEmployee.Result}");
 
