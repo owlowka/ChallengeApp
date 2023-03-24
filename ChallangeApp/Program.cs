d843695e45b37f775391d628e50b76bc38b1f302
owlowka <roksana.maria.duda@gmail.com>
2023-03-24 16:30:46 +0100

    Refactoring drugi zliczanie cyfr w liczbie.

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,26 +1,17 @@
 ﻿
-using System.Diagnostics.Metrics;
-
 int number = 4566;
 string numberInString = number.ToString();
 char[] letters = numberInString.ToArray();
+int[] digits = new int[10];
 
-List<char> digits = new List<char>();
-digits.Add('0');
-digits.Add('1');
-digits.Add('2');
-digits.Add('3');
-digits.Add('4');
-digits.Add('5');
-digits.Add('6');
-digits.Add('7');
-digits.Add('8');
-digits.Add('9');
-
-
-foreach (var digit in digits)
+foreach (char letter in letters)
 {
-    int digitCount = letters.Count(letter => letter == digit);
-        Console.WriteLine($"{digit} => {digitCount}");
+    int digit = int.Parse(letter.ToString());
+    digits[digit]++;
+}
+
+for (int i = 0;i < digits.Length; i++)
+{
+    Console.WriteLine($"{i} => {digits[i]}");
 }
 
