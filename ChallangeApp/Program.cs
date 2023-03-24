a05e09b8363d720398d83cb47a10faf6f6b67e22
owlowka <roksana.maria.duda@gmail.com>
2023-03-24 16:15:57 +0100

    Refaktoring zliczania cyfr w liczbie.

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,74 +1,26 @@
-﻿using System.Diagnostics.Metrics;
+﻿
+using System.Diagnostics.Metrics;
 
 int number = 4566;
 string numberInString = number.ToString();
 char[] letters = numberInString.ToArray();
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+List<char> digits = new List<char>();
+digits.Add('0');
+digits.Add('1');
+digits.Add('2');
+digits.Add('3');
+digits.Add('4');
+digits.Add('5');
+digits.Add('6');
+digits.Add('7');
+digits.Add('8');
+digits.Add('9');
 
-foreach (char letter in letters)
+
+foreach (var digit in digits)
 {
-        if (letter == '0')
-        {
-            counter0++;
-
-        }
-        else if (letter == '1')
-        {
-            counter1++;
-        }
-        else if (letter == '2')
-        {
-            counter2++;
-        }
-        else if (letter == '3')
-        {
-            counter3++;
-        }
-        else if (letter == '4')
-        {
-            counter4++;
-        }
-        else if (letter == '5')
-        {
-            counter5++;
-        }
-        else if (letter == '6')
-        {
-            counter6++;
-        }
-        else if (letter == '7')
-        {
-            counter7++;
-        }
-        else if (letter == '8')
-        {
-            counter8++;
-        }
-        else if (letter == '9')
-        {
-        counter9++;
-    }
+    int digitCount = letters.Count(letter => letter == digit);
+        Console.WriteLine($"{digit} => {digitCount}");
 }
-
-Console.WriteLine($"Dla liczby: {number}");
-Console.WriteLine($"0 => {counter0++}");
-Console.WriteLine($"1 => {counter1++}");
-Console.WriteLine($"2 => {counter2++}");
-Console.WriteLine($"3 => {counter3++}");
-Console.WriteLine($"4 => {counter4++}");
-Console.WriteLine($"5 => {counter5++}");
-Console.WriteLine($"6 => {counter6++}");
-Console.WriteLine($"7 => {counter7++}");
-Console.WriteLine($"8 => {counter8++}");
-Console.WriteLine($"9 => {counter9++}");
 
